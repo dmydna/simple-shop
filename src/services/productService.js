c5888cdfc3aeacf8be7ed44507ec84007a209d9e
dmydna <danimaydana9@gmail.com>
2025-12-20 17:02:09 -0300

    fix docker

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,5 +1,6 @@
 // src/services/productService.js
-const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
+
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
 
 export const productService = {
     // GET: Obtener todos los productos
